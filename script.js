f701a91fa28f965a35e01acece30d276d5113fb2
Kevin Hobbs <kevin21921@gmail.com>
2021-05-03 02:29:30 -0400

    Added javascript to hide elements when water consumption was sero.

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ const percentage = document.getElementById('percentage')
 
 const remained = document.getElementById('remained')
 
+updateBigCup()
+
 smallCups.forEach((cup, idx) => {
   cup.addEventListener('click', () => highlightCups(idx))
 })
 
 function highlightCups(idx) {
+  
   if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
     idx--
   }
@@ -22,4 +25,30 @@ function highlightCups(idx) {
       cup.classList.remove('full')
     }
   })
+
+  updateBigCup()
 }
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length
+  
+  const totalCups = smallCups.length
+
+  if(fullCups === 0) {
+    percentage.style.visibility = ''
+    percentage.style.height = ''
+    percentage.innerText = ''
+  }else {
+    percentage.style.visibility = 'visible'
+    percentage.innerText = `${(fullCups / totalCups)*100}%` 
+    percentage.style.height = `${(fullCups / totalCups)*330}px`
+
+    if(fullCups === totalCups) {
+      remained.style.visibility = ''
+      remained.style.height = 0
+    }
+  }
+
+
+}
+    
